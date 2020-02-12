7aa5dfb25c4ece2f1578ffb6105042a2c7dcf4bd
Mone20 <44345543+Mone20@users.noreply.github.com>
2020-02-12 18:34:38 +0400

    Add files via upload

---
+++ src/main/Degree.java
@@ -0,0 +1,18 @@
+public class Degree {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    private String degree;
+    public Degree(int i,String d)
+    {
+        this.id=i;
+        this.degree=d;
+    }
+}
---
+++ src/main/UniversityPosition.java
@@ -0,0 +1,18 @@
+public class UniversityPosition {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    private String position;
+    public UniversityPosition(int i,String p)
+    {
+        this.id=i;
+        this.position=p;
+    }
+}
