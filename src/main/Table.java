0e1cac9ffb3422f2c16fd3a072b779f984b01ee3
Mone20 <44345543+Mone20@users.noreply.github.com>
2020-02-12 18:38:06 +0400

    1

--- src/main/Table.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public interface Table<T> {
-int insert(T newInstance) throws SQLException;
-int delete(int id) throws SQLException;
-int update(int id, String nameColumn, String newInstance) throws SQLException;
-T select(int id) throws SQLException;
-}
