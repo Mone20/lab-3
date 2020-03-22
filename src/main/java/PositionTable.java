9d349ffcef3e1d1c9e68a48ca5ae3cdb9d190cc6
Mone20 <44345543+Mone20@users.noreply.github.com>
2020-03-22 16:14:21 +0400

    Revert "Position branch"

--- src/main/java/PositionTable.java
+++ src/main/java/PositionTable.java
@@ -1,19 +1,23 @@
-import model.Position;
+import model.Degree;
+import model.UniversityPosition;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class PositionTable implements Table<Position> {
-    private Connection connect = null;
-
-    public PositionTable(Connection connection) throws SQLException {
-        this.connect = Database.connection;
-        String SQL = " CREATE TABLE IF NOT EXISTS public.position\n" +
+public class PositionTable implements Table<UniversityPosition> {
+    private  Connection connect=null;
+    public PositionTable() throws SQLException {
+        this.connect=Database.connection;
+        String SQL=" CREATE TABLE IF NOT EXISTS public.position\n" +
                 "(\n" +
                 "    id integer NOT NULL,\n" +
-                "    position character varying(20) COLLATE pg_catalog.\"default\" NOT NULL,\n" +
+                "    position character varying(20) COLLATE pg_catalog.default NOT NULL,\n" +
                 "    CONSTRAINT position_pkey PRIMARY KEY (id)\n" +
                 ")\n" +
                 "\n" +
@@ -23,13 +27,13 @@ public class PositionTable implements Table<Position> {
                 "    OWNER to postgres;";
         PreparedStatement preparedStatement = connect.prepareStatement(SQL);
         preparedStatement.executeUpdate();
+
         System.out.println("Table successfully created..");
 
     }
+    public int insert(UniversityPosition position) throws SQLException {
 
-    public int insert(Position position) throws SQLException {
-
-        String sql = "INSERT INTO public.position(\n" +
+        String sql="INSERT INTO public.position(\n" +
                 "\tid, position)\n" +
                 "\tVALUES (?, ?);";
         PreparedStatement preparedStatement = connect.prepareStatement(sql);
@@ -40,70 +44,77 @@ public class PositionTable implements Table<Position> {
 
 
     public int delete(int id) throws SQLException {
-        String sql = "DELETE FROM public.workers\n" +
-                "\tWHERE positionId=?;";
-        PreparedStatement preparedStatement = connect.prepareStatement(sql);
-        preparedStatement.setInt(1, id);
-        preparedStatement.executeUpdate();
-       sql = "DELETE FROM public.position\n" +
+        Connection connect=null;
+        String sql="DELETE FROM public.position\n" +
                 "\tWHERE id=?;";
-         preparedStatement = connect.prepareStatement(sql);
-        preparedStatement.setInt(1, id);
-        preparedStatement.executeUpdate();
+        PreparedStatement preparedStatement = connect.prepareStatement(sql);
+        preparedStatement.setInt(1,id);
         return preparedStatement.executeUpdate();
-
     }
 
 
     public int update(int id, String nameColumn, String newInstance) throws SQLException {
 
-        String sql = "UPDATE public.position\n" +
-                "\tSET " + nameColumn + "=?\n" +
+        String sql="UPDATE public.position\n" +
+                "\tSET "+nameColumn+"=?\n" +
                 "\tWHERE id=?;";
         PreparedStatement preparedStatement = connect.prepareStatement(sql);
-        preparedStatement.setString(1, newInstance);
-        preparedStatement.setInt(2, id);
+        preparedStatement.setString(1,newInstance);
+        preparedStatement.setInt(2,id);
         return preparedStatement.executeUpdate();
+    }
+
+    @Override
+    public int update(HashMap<String, String> map, int id) throws SQLException {
+        return 0;
     }
 
     public int update(int id, String nameColumn, int newInstance) throws SQLException {
 
-        String sql = "UPDATE public.position\n" +
-                "\tSET " + nameColumn + "=?\n" +
+        String sql="UPDATE public.position\n" +
+                "\tSET "+nameColumn+"=?\n" +
                 "\tWHERE id=?;";
         PreparedStatement preparedStatement = connect.prepareStatement(sql);
-        preparedStatement.setInt(1, newInstance);
-        preparedStatement.setInt(2, id);
+        preparedStatement.setInt(1,newInstance);
+        preparedStatement.setInt(2,id);
         return preparedStatement.executeUpdate();
     }
-
-    public Position select(int id) throws SQLException {
-        String sql = "SELECT id, position\n" +
+    public UniversityPosition select(int id) throws SQLException {
+        String sql="SELECT id, position\n" +
                 "\tFROM public.position\n" +
                 "\tWHERE id=?;";
         PreparedStatement preparedStatement = connect.prepareStatement(sql);
-        preparedStatement.setInt(1, id);
-        ResultSet result = preparedStatement.executeQuery();
-        Position selectPosition = null;
-        if (result.next())
-            selectPosition = new Position(result.getInt("id"), result.getString("position"));
+        preparedStatement.setInt(1,id);
+        ResultSet result=preparedStatement.executeQuery();
+        UniversityPosition selectPosition=null;
+        if(result.next())
+        selectPosition=new UniversityPosition( result.getInt("id"),result.getString("position"));
 
         return selectPosition;
     }
 
-    public ResultSet selectAll() throws SQLException {
-        String sql = "SELECT id, position\n" +
-                "\tFROM public.position\n";
-        PreparedStatement preparedStatement = connect.prepareStatement(sql);
-
-        ResultSet result = preparedStatement.executeQuery();
-        return result;
-
+    @Override
+    public List<UniversityPosition> select(Map<String, String> map) throws SQLException {
+        return null;
     }
 
+    public List selectAll() throws SQLException {
+        String sql="SELECT id, position\n" +
+                "\tFROM public.position\n" ;
+        PreparedStatement preparedStatement = connect.prepareStatement(sql);
+
+        ResultSet result=preparedStatement.executeQuery();
+        ArrayList<UniversityPosition> listDTO=new ArrayList<UniversityPosition>();
+
+        while(result.next())
+            listDTO.add(new UniversityPosition( result.getInt("id"),result.getString("position")));
+        return  listDTO;
+
+
+    }
     @Override
     public void truncate() throws SQLException {
-        String sql = "TRUNCATE TABLE  public.position CASCADE ;";
+        String sql="TRUNCATE TABLE  public.position CASCADE ;";
         PreparedStatement preparedStatement = connect.prepareStatement(sql);
         preparedStatement.execute();
 
